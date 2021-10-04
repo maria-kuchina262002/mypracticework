c803e47846156de6ac796bb272292c7049ff3849
maria-kuchina262002 <mashakuchina2002@icloud.com>
2021-10-04 16:37:39 +0300

    Methods

---
+++ Controllers/RestaurantRKeeperClientController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace mypracticework.Controllers
+{
+    [ApiController]
+    [Route("/client")]
+    public class ClientController : ControllerBase
+    {
+
+        [HttpGet("InformationAboutDishes")]
+        public string InformationAboutDishes(string str)
+        {
+            return str; // Метод получения информации о блюдах
+        }
+
+        [HttpGet("OrderingDishes")]
+        public string OrderingDishes(string str)
+        {
+            return str; // Метод заказа блюд
+        }
+       
+        [HttpGet("PayBill")]
+        public string PayBill(string str)
+        {
+            return str; // Метод оплаты счета
+        }
+
+        [HttpGet("LeavingReview")]
+        public string LeavingReview (string str)
+        {
+            return str; // Метод оценивания обслуживания и блюд
+        }
+    }
+}
+
