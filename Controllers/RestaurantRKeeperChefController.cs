8b464858b4ccf19c559d0e63fa834ef7dc9f292d
maria-kuchina262002 <91078274+maria-kuchina262002@users.noreply.github.com>
2021-10-21 15:06:59 +0300

    Update RestaurantRKeeperChefController.cs

--- Controllers/RestaurantRKeeperChefController.cs
+++ Controllers/RestaurantRKeeperChefController.cs
@@ -12,25 +12,25 @@ namespace mypracticework.Controllers
     public class ClientController : ControllerBase
     {
 
-        [HttpGet("TakingOrder")]
+        [HttpGet("taking-order")]
         public string InformationAboutDishes(string str)
         {
             return str; // Метод принятия заказа
         }
 
-        [HttpGet("Cooking")]
+        [HttpGet("cooking")]
         public string OrderingDishes(string str)
         {
             return str; // Метод приготовления заказа
         }
 
-        [HttpPost("MenuCompilation")]
+        [HttpPost("menu-compilation")]
         public string PayBill(string str)
         {
             return str; // Метод составления меню
         }
 
-        [HttpPost("CompilationMissingProducts")]
+        [HttpPost("compilation-missing-products")]
         public string LeavingReview(string str)
         {
             return str; // Метод формирования списка недостающих продуктов
