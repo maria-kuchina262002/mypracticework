6cd107aadfe8ee384bdbe90976189ee6fb46abc7
maria-kuchina262002 <mashakuchina2002@icloud.com>
2021-10-31 22:29:24 +0300

    Create RestaurantRKeeperChefController.cs

---
+++ Controllers/RestaurantRKeeperChefController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace mypracticework.Controllers
+{
+    [ApiController]
+    [Route("/chef")]
+    public class ClientController : ControllerBase
+    {
+
+        [HttpGet("TakingOrder")]
+        public string InformationAboutDishes(string str)
+        {
+            return str; // Метод принятия заказа
+        }
+
+        [HttpGet("Cooking")]
+        public string OrderingDishes(string str)
+        {
+            return str; // Метод приготовления заказа
+        }
+
+        [HttpPost("MenuCompilation")]
+        public string PayBill(string str)
+        {
+            return str; // Метод составления меню
+        }
+
+        [HttpPost("CompilationMissingProducts")]
+        public string LeavingReview(string str)
+        {
+            return str; // Метод формирования списка недостающих продуктов
+        }
+    }
+}
