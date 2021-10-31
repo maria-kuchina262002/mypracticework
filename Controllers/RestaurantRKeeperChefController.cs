431d013a9f298069b100f30dd9bdef735fd72405
maria-kuchina262002 <91078274+maria-kuchina262002@users.noreply.github.com>
2021-10-31 18:25:26 +0300

    Update RestaurantRKeeperChefController.cs

--- Controllers/RestaurantRKeeperChefController.cs
+++ Controllers/RestaurantRKeeperChefController.cs
@@ -8,30 +8,30 @@ using Microsoft.Extensions.Logging;
 namespace mypracticework.Controllers
 {
     [ApiController]
-    [Route("/chef")]
+    [Route("/Chef")]
     public class ClientController : ControllerBase
     {
 
-        [HttpGet("taking-order")]
-        public string InformationAboutDishes(string str)
+        [HttpGet("TakingOrder")]
+        public string TakingOrder(string str)
         {
             return str; // Метод принятия заказа
         }
 
-        [HttpGet("cooking")]
-        public string OrderingDishes(string str)
+        [HttpGet("Cooking")]
+        public string Cooking(string str)
         {
             return str; // Метод приготовления заказа
         }
 
-        [HttpPost("menu-compilation")]
-        public string PayBill(string str)
+        [HttpPost("MenuCompilation")]
+        public string MenuCompilation(string str)
         {
             return str; // Метод составления меню
         }
 
-        [HttpPost("compilation-missing-products")]
-        public string LeavingReview(string str)
+        [HttpPost("CompilationMissingProducts")]
+        public string CompilationMissingProducts(string str)
         {
             return str; // Метод формирования списка недостающих продуктов
         }
