2b1f7ad17e9e73f273a2b71fd073d759cb05d459
maria-kuchina262002 <mashakuchina2002@icloud.com>
2021-11-01 11:43:44 +0300

    CRUD

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace mypracticework.Controllers
+{
+    [ApiController]
+    [Route("/Products")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpPut("Create")]
+        public string Create(string str)
+        {
+            return str;
+        }
+
+        [HttpGet("Read")]
+        public string Read(string str)
+        {
+            return str;
+        }
+
+        [HttpPatch("Update")]
+        public string Update(string str)
+        {
+            return str;
+        }
+
+        [HttpDelete("Delete")]
+        public string Delete(string str)
+        {
+            return str;
+        }
+    }
+
+}
