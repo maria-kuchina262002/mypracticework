4693ec5e20eba24ebe49bd7ec2beacb67bdd06c2
maria-kuchina262002 <91078274+maria-kuchina262002@users.noreply.github.com>
2021-11-17 18:49:16 +0300

    Add files via upload

---
+++ Domain/Меню.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace mypracticework.Domain
+{
+    public class Меню
+    {
+        public int КодБлюда { get; set; }
+        public int Блюдо { get; set; }
+        public object Фотография { get; set; } //
+        public string Ингридиент { get; set; }
+        public int Порция { get; set; }
+        public decimal Цена { get; set; }
+
+    }
+}
