b1e234f4b5c8fba549461ab610f4596286d9b3f8
maria-kuchina262002 <mashakuchina2002@icloud.com>
2021-11-18 13:15:54 +0300

    Fixed compiler errors

--- Domain/Ingridient.cs
+++ Domain/Ingridient.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace mypracticework.Domain
 {
     class Ingridient
--- Domain/Review.cs
+++ Domain/Review.cs
@@ -3,7 +3,7 @@ namespace mypracticework.Domain
     class Review
     {
         public int IDReview { get; set; }
-        public string Review { get; set; }
+        public string Content { get; set; }
         public string Comment { get; set; }
     }
 }
